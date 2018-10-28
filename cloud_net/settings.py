2793d02e397f3cd2cfee545a57057e5a01b972d0
Victor Sui <victorsui11@gmail.com>
2018-10-28 14:52:27 -0500

    Use environment variables for settings

--- cloud_net/settings.py
+++ cloud_net/settings.py
@@ -76,21 +76,27 @@ WSGI_APPLICATION = 'cloud_net.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+if 'RDS_HOSTNAME' not in os.environ:
+    except ValueError('Environment not set up correctly')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cloud_net',
-        'USER': 'cs436',
-        'PASSWORD': 'kingcaesar',
-        'HOST': '####',
-        'PORT': '3306'
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'] 
     }
 }
 
-CACHEDS = {
+CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '####:11211',
+        'BACKEND': 'django_elasticache.memcached.ElastiCache',
+        'LOCATION': os.environ['CACHE_LOCATION'],
+        'OPTIONS': {
+            'IGNORE_CLUSTER_ERRORS': [True, False]
+        }   
     }
 }
 
