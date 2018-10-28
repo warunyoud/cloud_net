a0460d89e4d0a0605b580d1e4a028b7f9a76a19c
Victor Sui <victorsui11@gmail.com>
2018-10-28 17:51:14 -0500

    Correct typo

--- cloud_net/settings.py
+++ cloud_net/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     }
 }
 
-CACHEDS = {
+CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': 'memcached:11211',
